5b453ef5b12ac9b4b4640cc1edbdef9ba457aa69
shivakumar <shivakumar@shivakumars-MacBook-Air.local>
2020-02-17 21:04:57 +0530

    modified the preloader component which will be shown on submit click

--- src/container/searchbooks/index.js
+++ src/container/searchbooks/index.js
@@ -11,34 +11,47 @@ const SearchBooks = () => {
     const [loading, setLoading] = useState(false)
 
     const preLoader = () => {
-        return <div style={{margin: '1em',
-            padding: '0.5em',
-            border: '1px solid #D7D7D7',
-            boxShadow: '1px 1px 6px 0px #D7D7D7'}}>
-            <ContentLoader
-                speed={2}
-                width={400}
-                height={100}
-                backgroundColor="#f3f3f3"
-                foregroundColor="#ecebeb"
-            >
-                <rect x="0" y="8" rx="3" ry="3" width="88" height="6" />
-                <rect x="0" y="26" rx="3" ry="3" width="52" height="6" />
-                <rect x="0" y="56" rx="3" ry="3" width="410" height="6" />
-                
-            </ContentLoader>
+        return <div style={{
+            display: 'flex',
+            flexWrap: 'wrap'
+        }}>
+            {
+                [1, 2].map(() => (
+                    <div style={{
+                        margin: '1em',
+                        padding: '0.5em',
+                        border: '1px solid #D7D7D7',
+                        boxShadow: '1px 1px 6px 0px #D7D7D7'
+                    }}>
+                        <ContentLoader
+                            speed={2}
+                            width={300}
+                            height={100}
+                            backgroundColor="#f3f3f3"
+                            foregroundColor="#ecebeb"
+                        >
+                            <rect x="100" y="8" rx="3" ry="3" width="88" height="6" />
+                            <rect x="50" y="35" rx="3" ry="3" width="200" height="20" />
+                            <rect x="100" y="80" rx="3" ry="3" width="100" height="6" />
+
+                        </ContentLoader>
+                    </div>
+                ))
+            }
         </div>
+
     }
 
-    const _getResults = async ({ id = ''}) => {
+    const _getResults = async ({ id = '' }) => {
+        //calling api which fetches summary,title,Author for the selected
         setLoading(true);
         let response = await searchBooks({
             id
         })
         response && setSearchResults([
-            
+
             ...searchResults,
-            ...response['summaries'].filter(({id}) => searchResults.every(sumry => sumry['id'] !== id))
+            ...response['summaries'].filter(({ id }) => searchResults.every(sumry => sumry['id'] !== id))
         ])
         setLoading(false);
     }
