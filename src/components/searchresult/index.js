d48e56c9a47860623a1376434bbe1d9a85cc2e6f
shivakumarbetel <shivakumar.betel@gmail.com>
2020-02-19 12:49:50 +0530

    showing message if no book selected

--- src/components/searchresult/index.js
+++ src/components/searchresult/index.js
@@ -8,13 +8,12 @@ import './search-result.css';
 
 const SearchResults = props => {
     const { searchResults = [] } = props
-console.log("searchResults",searchResults)
     return (
         <div data-testid="results" className="resultCon">
             {
-                searchResults.map(result => <Card key={result['id']} author={result['author']}
+              searchResults.length >0 ? searchResults.map(result => <Card key={result['id']} author={result['author']}
                                                    title={result['title']}
-                                                   summary={result['summary']}/>)
+                                                   summary={result['summary']}/>) : 'No Books Selected'
             }
         </div>
     )
